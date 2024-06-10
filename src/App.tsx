9f4a516b8faf5042b94dcf588edc7ed409682678
Nugar <cladeadenugar@gmail.com>
2024-06-10 09:29:03 +0100

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
 import Heading from "./components/Heading";
+import Section from "./components/Section";
+import List from "./components/List";
 
 const App = () => {
+  const [count, setCount] = useState<number>(1);
   return (
     <div>
       <Heading title={"Hello World"} />
+      <Section title={"True title"}>This is my section</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={["one", "two", "three"]}
+        render={(item: string) => <span className="gold">{item}</span>}
+      />
     </div>
   );
 };
